d9c15d731728828bb1c7b0987195d92a15333259
iggy228 <i.borovy@gmail.com>
2021-05-16 13:00:00 +0200

    Make basic app with 3 fragments and navigation.

---
+++ app/src/main/java/sk/iggy/vaccineit/MainActivity.kt
@@ -0,0 +1,17 @@
+package sk.iggy.vaccineit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+}
