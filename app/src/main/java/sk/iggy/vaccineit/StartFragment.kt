00dd54080b6eddb1cf3b1fac4e597334ab890045
iggy228 <i.borovy@gmail.com>
2021-05-18 19:57:30 +0200

    Make UserViewModel and applying it in StartFragment.

--- app/src/main/java/sk/iggy/vaccineit/StartFragment.kt
+++ app/src/main/java/sk/iggy/vaccineit/StartFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import sk.iggy.vaccineit.databinding.FragmentStartBinding
+import sk.iggy.vaccineit.model.UserViewModel
 
 class StartFragment : Fragment() {
     private lateinit var binding: FragmentStartBinding
+    private val sharedViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +25,12 @@ class StartFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            viewModel = sharedViewModel
+        }
+
         binding.nextButton.setOnClickListener { goToNextScreen() }
     }
 
---
+++ app/src/main/java/sk/iggy/vaccineit/model/UserViewModel.kt
@@ -0,0 +1,47 @@
+package sk.iggy.vaccineit.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel: ViewModel() {
+    private val _id = MutableLiveData("")
+    val id: LiveData<String> get() = _id
+
+    private val _fullname = MutableLiveData("")
+    val fullname: LiveData<String> get() = _fullname
+
+    private val _email = MutableLiveData("")
+    val email: LiveData<String> get() = _email
+
+    private val _phone = MutableLiveData("")
+    val phone: LiveData<String> get() = _phone
+
+    private val _vaccineType = MutableLiveData("")
+    val vaccineType: LiveData<String> get() = _vaccineType
+
+    private val _date = MutableLiveData("")
+    val date: LiveData<String> get() = _date
+
+    fun setId(userId: String) {
+        if (userId.length == 10) {
+            _id.value = userId
+        }
+    }
+
+    fun setFullname(firstName: String, lastname: String) {
+        _fullname.value = "$firstName $lastname"
+    }
+
+    fun setEmail(email: String) {
+        _email.value = email
+    }
+
+    fun setPhone(phone: String) {
+        _phone.value = phone
+    }
+
+    fun setDate(date: String) {
+        _date.value = date
+    }
+}
